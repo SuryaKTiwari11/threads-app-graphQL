13d1a26ebcd699b2a0462e7b2eb0cc85aa6a8eff
SuryaKantTiwari <jamcocobutter@gmail.com>
2025-10-17 23:06:29 +0530

    graphQL server setup done

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+import cors from "cors";
+
+const port = process.env.PORT || 8000;
+
+const typeDefs = `
+  type Query {
+    ping: String
+    say(name:String):String
+  }
+`;
+const resolvers = {
+  Query: {
+    ping: () => "pong",
+    say: ({ name }: { name: string }) => `Hey ${name}, how are you`,
+  },
+};
+const gqlServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await gqlServer.start();
+app.use(cors(), express.json());
+app.use("/graphql", expressMiddleware(gqlServer));
+
+app.get("/", (req, res) => {
+  res.json({ message: "server is on" });
+});
+
+app.listen(port, () => console.log(` server is listening on port ${port}`));
